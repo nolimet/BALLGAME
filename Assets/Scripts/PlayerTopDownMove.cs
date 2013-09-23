0ade57623be0aac4bc565612329cede49b8ed331
Nolimet <nolimet.stamdata.nl>
2013-09-23 12:15:50 +0200

    added moscow and changed movement still wip

--- Assets/Scripts/PlayerTopDownMove.cs
+++ Assets/Scripts/PlayerTopDownMove.cs
@@ -10,8 +10,28 @@ public class PlayerTopDownMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		rigidbody.AddTorque(0,Input.GetAxis("Horizontal"),0);
-		rigidbody.AddForce(Input.GetAxis("Vertical"),0,0);
+		transform.Rotate(0,Input.GetAxis("Horizontal")*2,0);
+		rigidbody.AddRelativeForce(Input.GetAxis("Vertical")*10,0,0);
 		
+	}
+	
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.collider.gameObject.name=="NorthWall")
+		{
+			transform.position=transform.position.x*-1;
+		}
+		if(col.collider.gameObject.name=="SouthWall")
+		{
+			transform.position=transform.position.x*-1;
+		}
+		if(col.collider.gameObject.name=="WestWall")
+		{
+			transform.position=transform.position.z*-1;
+		}
+		if(col.collider.gameObject.name=="EastWall")
+		{
+			transform.position=transform.position.z*-1;
+		}
 	}
 }
